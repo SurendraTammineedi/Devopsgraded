7634b1a103487c9827f11676e92eba6795831a7c
SurendraTammineedi <tammineedisurendra@gmail.com>
2021-11-24 20:08:59 +0530

    firstcommit

---
+++ hellodevops/src/main/java/com/hcl/devops/HellodevopsApplication.java
@@ -0,0 +1,14 @@
+package com.hcl.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellodevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellodevopsApplication.class, args);
+		System.out.println("hellodevops");
+	}
+
+}
